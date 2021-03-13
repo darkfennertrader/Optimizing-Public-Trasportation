ab9c9aa1a4920d9313208acdd8b17cbdd688cdc1
darkfennertrader <darkfenner69@gmail.com>
2021-03-13 17:48:26 +0100

    minor adjustment on auto.offset.reset

--- consumers/consumer.py
+++ consumers/consumer.py
@@ -34,7 +34,7 @@ class KafkaConsumer:
         self.broker_properties = {
             "group.id": "0",
             "bootstrap.servers": "PLAINTEXT://localhost:9092",
-            "auto.offset.reset": "earliest",
+            "auto.offset.reset": "earliest" if offset_earliest else "latest",
         }
 
         # TODO: Create the Consumer, using the appropriate type.
